f0d2489873e319228355fad8bb5fe8559372c03c
Rosone <Roynersimon45mail.com>
2024-10-21 09:22:15 -0500

    Tercer commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title> Login and Registration Form in HTML & CSS | CodingLab </title>
+    <title>Choco Travel - Registrate y Conoce el Choco </title>
     <link rel="stylesheet" href="{{ url('assets/css/register.css') }}">
     <!-- Fontawesome CDN Link -->
+     <link rel="shortcut icon" href={{url('assets/images/logo/logo.png')}} type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
@@ -14,16 +15,18 @@
     <div class="container">
         <input type="checkbox" id="flip">
         <div class="cover">
-            <div class="front">
+            <div class="back">
+            <img src={{url('assets/images/gallary/back.jpg')}} alt="">
                 <div class="text">
-                    <span class="text-1"><br></span>
-                    <span class="text-2"></span>
+                <span class="text-1">Registrate <br> Y conocer lo mejor del Chocó</span>
+                    <span class="text-2">Acandi</span>
                 </div>
             </div>
-            <div class="back">
+            <div class="front">
+            <img src={{url('assets/images/gallary/front.jpg')}} alt="">
                 <div class="text">
-                    <span class="text-1"><br> </span>
-                    <span class="text-2"></span>
+                    <span class="text-1">Logueate <br>y conoce lo mejor del Chocó</span>
+                    <span class="text-2">Nuqui</span>
                 </div>
             </div>
         </div>
@@ -32,53 +35,53 @@
             <div class="form-content">
                 <!-- Login Form -->
                 <div class="login-form">
-                    <div class="title">Login</div>
+                    <div class="title">Logueate</div>
                     <form action="{{ route('login') }}" method="POST">
                         @csrf <!-- Token CSRF requerido por Laravel para formularios POST -->
                         <div class="input-boxes">
                             <div class="input-box">
                                 <i class="fas fa-envelope"></i>
-                                <input type="email" name="email" placeholder="Enter your email" required>
+                                <input type="email" name="email" placeholder="Ingresa tu correo" required>
                             </div>
                             <div class="input-box">
                                 <i class="fas fa-lock"></i>
-                                <input type="password" name="password" placeholder="Enter your password" required>
+                                <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
                             </div>
-                            <div class="text"><a href="#">Forgot password?</a></div>
+                            <div class="text"><a href="#"></a></div>
                             <div class="button input-box">
-                                <input type="submit" value="Submit">
+                                <input type="submit" value="Ingresa">
                             </div>
-                            <div class="text sign-up-text">Don't have an account? <label for="flip">Signup now</label></div>
+                            <div class="text sign-up-text">No tienes Cuenta? <label for="flip">Registrate</label></div>
                         </div>
                     </form>
                 </div>
 
                 <!-- Signup Form -->
                 <div class="signup-form">
-                    <div class="title">Signup</div>
+                    <div class="title">Registrate</div>
                     <form action="{{ route('register') }}" method="POST">
                         @csrf <!-- Token CSRF requerido por Laravel para formularios POST -->
                         <div class="input-boxes">
                             <div class="input-box">
                                 <i class="fas fa-user"></i>
-                                <input type="text" name="name" placeholder="Enter your name" required>
+                                <input type="text" name="name" placeholder="Ingresa tu nombre" required>
                             </div>
                             <div class="input-box">
                                 <i class="fas fa-envelope"></i>
-                                <input type="email" name="email" placeholder="Enter your email" required>
+                                <input type="email" name="email" placeholder="Ingresa tu coreo" required>
                             </div>
                             <div class="input-box">
                                 <i class="fas fa-lock"></i>
-                                <input type="password" name="password" placeholder="Enter your password" required>
+                                <input type="password" name="password" placeholder="Ingresa tu contraseña" required>
                             </div>
                             <div class="input-box">
                                 <i class="fas fa-lock"></i>
-                                <input type="password" name="password_confirmation" placeholder="Confirm your password" required>
+                                <input type="password" name="password_confirmation" placeholder="Confirma tu contraseña" required>
                             </div>
                             <div class="button input-box">
                                 <input type="submit" value="Submit">
                             </div>
-                            <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+                            <div class="text sign-up-text">Ya cuentas con una cuenta? <label for="flip">Logueate</label></div>
                         </div>
                     </form>
                 </div>
